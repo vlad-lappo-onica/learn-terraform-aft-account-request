18c9e2891c71832b4e4f7aa8a1960fb452a583a4
Vlad Lappo <vlad.lappo@gmail.com>
2022-02-14 13:55:10 -0500

    added dev 02

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,24 @@
-module "sandbox" {
+module "dev_account_02" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
+    AccountEmail = "vlad.lappo+dev02@gmail.com"
+    AccountName  = "dev-account-02"
+    # Syntax for top-level OU
+    ManagedOrganizationalUnit = "Dev"
+    # Syntax for nested OU
+    # ManagedOrganizationalUnit = "Sandbox (ou-xfe5-a8hb8ml8)"
+    SSOUserEmail     = "vlad.lappo@gmail.com"
+    SSOUserFirstName = "Vlad"
+    SSOUserLastName  = "Lappo"
   }
 
   account_tags = {
-    "Learn Tutorial" = "AFT"
+    "ABC:Division"    = "ENT"
+    "ABC:Environment" = "Dev"
+    "ABC:CostCenter"  = "123456"
   }
+
 
   change_management_parameters = {
     change_requested_by = "HashiCorp Learn"
@@ -23,5 +29,5 @@ module "sandbox" {
     group = "non-prod"
   }
 
-  account_customizations_name = "sandbox"
+  /* account_customizations_name = "sandbox" */
 }
